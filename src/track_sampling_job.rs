c0df462b37f6342ee872c4aa15c99f760b98b2cc
Nan <email@fenqi.moe>
2024-04-11 10:19:27 +0800

    track sampling

---
+++ src/track_sampling_job.rs
@@ -0,0 +1,135 @@
+use std::fmt::Debug;
+use std::rc::Rc;
+use std::sync::Arc;
+
+use crate::base::{OzzError, OzzObj};
+use crate::math::f32_clamp_or_max;
+use crate::track::{Track, TrackValue};
+
+#[derive(Debug)]
+pub struct TrackSamplingJob<V, T = Rc<Track<V>>>
+where
+    V: TrackValue,
+    T: OzzObj<Track<V>>,
+{
+    track: Option<T>,
+    ratio: f32,
+    result: V,
+}
+
+pub type TrackSamplingJobRef<'t, V> = TrackSamplingJob<V, &'t Track<V>>;
+pub type TrackSamplingJobRc<V> = TrackSamplingJob<V, Rc<Track<V>>>;
+pub type TrackSamplingJobArc<V> = TrackSamplingJob<V, Arc<Track<V>>>;
+
+impl<V, T> Default for TrackSamplingJob<V, T>
+where
+    V: TrackValue,
+    T: OzzObj<Track<V>>,
+{
+    fn default() -> TrackSamplingJob<V, T> {
+        return TrackSamplingJob {
+            track: None,
+            ratio: 0.0,
+            result: V::default(),
+        };
+    }
+}
+
+impl<V, T> TrackSamplingJob<V, T>
+where
+    V: TrackValue,
+    T: OzzObj<Track<V>>,
+{
+    /// Gets track of `TrackSamplingJob`.
+    #[inline]
+    pub fn track(&self) -> Option<&T> {
+        return self.track.as_ref();
+    }
+
+    /// Sets track of `TrackSamplingJob`.
+    ///
+    /// Track to sample.
+    #[inline]
+    pub fn set_track(&mut self, track: T) {
+        self.track = Some(track);
+    }
+
+    /// Clears track of `TrackSamplingJob`.
+    #[inline]
+    pub fn clear_track(&mut self) {
+        self.track = None;
+    }
+
+    /// Gets ratio of `TrackSamplingJob`.
+    #[inline]
+    pub fn ratio(&self) -> f32 {
+        return self.ratio;
+    }
+
+    /// Sets ratio of `TrackSamplingJob`.
+    ///
+    /// Ratio used to sample track, clamped in range 0.0-1.0 before job execution.
+    /// 0 is the beginning of the track, 1 is the end.
+    /// This is a ratio rather than a ratio because tracks have no duration.
+    #[inline]
+    pub fn set_ratio(&mut self, ratio: f32) {
+        self.ratio = f32_clamp_or_max(ratio, 0.0f32, 1.0f32);
+    }
+
+    /// Gets **output** result of `TrackSamplingJob`.
+    #[inline]
+    pub fn result(&self) -> V {
+        return self.result;
+    }
+
+    /// Clears result of `TrackSamplingJob`.
+    #[inline]
+    pub fn clear_result(&mut self) {
+        self.result = V::default();
+    }
+
+    /// Clears all outputs of `TrackSamplingJob`.
+    #[inline]
+    pub fn clear_outs(&mut self) {
+        self.clear_result();
+    }
+
+    /// Validates `TrackSamplingJob` parameters.
+    #[inline]
+    pub fn validate(&self) -> bool {
+        return self.track.is_some();
+    }
+
+    /// Runs track sampling job's task.
+    /// The validate job before any operation is performed.
+    pub fn run(&mut self) -> Result<(), OzzError> {
+        let track = self.track.as_ref().ok_or(OzzError::InvalidJob)?.obj();
+
+        if track.key_count() == 0 {
+            self.result = V::default();
+            return Ok(());
+        }
+
+        let id1 = track
+            .ratios()
+            .iter()
+            .position(|&x| self.ratio < x)
+            .unwrap_or(track.key_count());
+        let id0 = id1.saturating_sub(1);
+
+        let id0_step = (track.steps()[id0 / 8] & (1 << (id0 & 7))) != 0;
+        if id0_step || id1 == track.key_count() {
+            self.result = track.values()[id0];
+        } else {
+            let tk0 = track.ratios()[id0];
+            let tk1 = track.ratios()[id1];
+            let t = (self.ratio - tk0) / (tk1 - tk0);
+            let v0 = track.values()[id0];
+            let v1 = track.values()[id1];
+            self.result = V::lerp(v0, v1, t);
+        }
+
+        return Ok(());
+    }
+}
+
